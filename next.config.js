15852f1ea068c79b648a87cd237774384412e409
nsLittle <notsolittle88@gmail.com>
2025-01-29 08:34:40 -0500

    next.config.js updated

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "media.newyorker.com",
+        },
+        {
+          protocol: "https",
+          hostname: "upload.wikimedia.org",
+        },
+        {
+          protocol: "https",
+          hostname: "news.mit.edu",
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
