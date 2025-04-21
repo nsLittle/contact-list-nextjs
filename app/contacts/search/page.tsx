b181705ebf2480c605de531de22d363c486bfda4
nsLittle <notsolittle88@gmail.com>
2025-04-21 12:46:46 -0400

    TS error fixed

--- app/contacts/search/page.tsx
+++ app/contacts/search/page.tsx
@@ -8,7 +8,7 @@ export default function SearchContacts() {
     <>
       <Header />
       <h2>Search Contacts</h2>
-      <SearchBar contacts={contacts} />
+      <SearchBar contacts={contacts as any} />
     </>
   );
 }
